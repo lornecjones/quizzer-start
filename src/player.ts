055eafb309998ed254896d6cb878a0f3f74241e9
Rockncoder <rockncoder@gmail.com>
2016-04-23 15:57:48 -0700

    Adds the calls for prev/next

--- src/player.ts
+++ src/player.ts
@@ -43,19 +43,32 @@ class Position {
   }
 }
 
+
 // annotations come from TS
 @Component({
   selector: 'player',
   templateUrl: './templates/player.html',
-  providers: [QuizService]
+  directives: [ROUTER_DIRECTIVES],
+  providers: [QuizService, ROUTER_PROVIDERS]
 })
 
 // classes come from es6
 export class PlayerComponent {
-  quiz: IQuizList;
+  questions:IQuizList;
+  answers:Array<boolean[]> = [];  //boolean[][]
   position:Position;
+  player:any;
+  showAnswers:boolean = false;
+  index = 0;
+  total = 10;
+  right = 0;
+  percent = 0;
+  responses = [];
+  title = "";
+  tagLine = "";
+  current:IQuestion;
 
-  constructor(private _quizService:QuizService, private _location:Location, private _routeParams: RouteParams) {
+  constructor(private _quizService:QuizService, private _location:Location, private _routeParams:RouteParams) {
     this.position = new Position();
   }
 
@@ -65,7 +78,41 @@ export class PlayerComponent {
 
   getQuiz() {
     let id = +this._routeParams.get('id');
-    this.quiz = this._quizService.getQuiz(id);
+    let data = this._quizService.getQuiz(id);
+    this.questions = data;
+    this.title = data.title;
+    this.tagLine = data.tagLine;
+    this.current = data.quiz.questions[0];
+    this.total = data.quiz.questions.length;
+
+    this.position.setMax(data.quiz.questions.length);
+    this.position.seek(Seek.Beginning);
+//    this.seekToQuestion(Seek.Beginning);
+
+    console.info(`Received data from service: ${data.quiz.questions.length}`);
   }
+
+  seekToQuestion(direction:Seek) {
+  }
+
+  tabulate() {
+
+  }
+
+
+  // the 'prev' button was clicked, move to previous question
+  previous = () => this.seekToQuestion(Seek.Backward);
+
+  // The 'next' button was clicked, move to the next question
+  next = () => this.seekToQuestion(Seek.Forward);
+
+  // The 'score' button was clicked, let's tabulate the answers
+  score = () => {
+    this.seekToQuestion(Seek.Score);
+    this.right = this.tabulate();
+    this.showAnswers = true;
+    this.seekToQuestion(Seek.Beginning);
+  };
+
 }
 
